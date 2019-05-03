369dc4dd1ab5bca481b77b51f1a4f627b0827701
Daeen Choi <exntu-metatron@googlegroups.com>
2019-05-03 21:15:00 +0900

    fix logic

--- projects/easy/두_정수_사이_합.py
+++ projects/easy/두_정수_사이_합.py
@@ -6,15 +6,17 @@ def add_numbers_in_between(num1, num2):
     if num1 > num2:
         num1, num2 = num2, num1
 
-    result = 0
-
-    for x in range(num1, num2+1):
-        result+=x
+    result = sum(range(num1, num2+1))
+    # for x in range(num1, num2+1):
+    #     result+=x
     return result
 
+
+
 def main():
-    print(add_numbers_in_between(5,1))
-    # 1 + 2 + 3 = 6
+    a = int(input("first number:\n"))
+    b = int(input("second number:\n"))
+    print('answer = {}'.format(add_numbers_in_between(a,b)))
 
 if __name__ == '__main__':
     main()
