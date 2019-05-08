9153a34b2bdd0a57224a38933dcbec42e4b45d45
paige0701 <paigechoi0701@gmail.com>
2019-05-09 00:04:19 +0900

    change logic: sum of divisors

--- projects/easy/약수의_합.py
+++ projects/easy/약수의_합.py
@@ -1,9 +1,15 @@
 def sum_of_divisors(s):
-    result = 0
-    for i in range(1, s+1):
-        if s%i == 0:
-            result+=i
-    return result
+    # result = 0
+    # for i in range(1, s+1):
+    #     if s%i == 0:
+    #         result+=i
+    # return result
+
+    # 약수는 증간까지만 룹을 돌면 된다.
+    # 12 일 경우 어짜피 12의 약수는 6까지 밖에 없다
+
+    # this is such a clever code !
+    return s + sum([i for i in range(1,(s//2)+1) if s%i == 0])
 
 
 
