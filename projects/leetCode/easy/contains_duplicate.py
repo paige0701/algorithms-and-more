7ea4335c984e58d4a463ddda68717bd8ad8babc0
paige0701 <paigechoi0701@gmail.com>
2019-06-27 08:52:58 +0900

    improve contains duplicate algoritm

--- projects/leetCode/easy/contains_duplicate.py
+++ projects/leetCode/easy/contains_duplicate.py
@@ -1,20 +1,38 @@
-def main(nums):
-    if len(nums) == 0:
-        return False
-    else:
-        map1 = {}
+import collections
 
-        count = 0
-        result = False
-        while count < len(nums):
-            key = str(nums[count])
-            if map1.get(key) is None:
-                map1[key] = key
-                count = count + 1
-            else:
-                result = True
-                break
-        return result
+
+def main(nums):
+
+    # if len(nums) == 0:
+    #     return False
+    # else:
+    #     map1 = {}
+    #
+    #     count = 0
+    #     result = False
+    #     while count < len(nums):
+    #         key = str(nums[count])
+    #         if map1.get(key) is None:
+    #             map1[key] = key
+    #             count = count + 1
+    #         else:
+    #             result = True
+    #             break
+    #     return result
+
+    # using collections
+    # a = nums
+    # b = collections.Counter(a).most_common()
+    # return True if b[0][1] > 1 else False
+
+    #using set
+    # nums.sort()
+    # aSet = list(set(nums))
+    # aSet.sort()
+    # return aSet != nums
+
+    # fastest
+    return len(nums) != len(set(nums))
 
 
 if __name__ == '__main__':
