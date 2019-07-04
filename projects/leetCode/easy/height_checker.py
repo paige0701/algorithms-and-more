2ca6433c71057b433b4ad868b9193fa2b8d71b9f
paige0701 <paigechoi0701@gmail.com>
2019-07-04 22:47:25 +0900

    solve algorithm : height_checker

---
+++ projects/leetCode/easy/height_checker.py
@@ -0,0 +1,24 @@
+from copy import deepcopy
+
+class Solution:
+
+
+    def height_checker(self, heights):
+
+        cloned_height = deepcopy(heights)
+        cloned_height = heights[:] # this is faster than deep copy
+        cloned_height = sorted(heights) # this is fastest !
+
+        heights.sort()
+        count = 0
+
+        for i in range(len(heights)):
+            if heights[i] != cloned_height[i]:
+                count +=1
+
+        return count
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.height_checker([1,1,4,2,1,3]))
+
