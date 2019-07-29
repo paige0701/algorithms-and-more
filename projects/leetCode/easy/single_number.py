6a6a1994438c19e39c2e7d0188c04f47eb4168f4
paige0701 <paigechoi0701@gmail.com>
2019-07-29 21:28:57 +0900

    solve algorithm: find single number

--- projects/leetCode/easy/single_number.py
+++ projects/leetCode/easy/single_number.py
@@ -1,1 +1,27 @@
 # Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+from collections import Counter, OrderedDict
+
+
+def find_single_number(nums):
+
+
+
+    # using bit manipulation
+    # a = 0
+    # for i in nums:
+    #     a^=i
+    # return a
+
+    duplicated_list = []
+    for i in nums:
+        if i not in duplicated_list:
+            duplicated_list.append(i)
+        else:
+            duplicated_list.remove(i)
+    return duplicated_list.pop()
+
+
+
+
+if __name__ == '__main__':
+    print(find_single_number([4,1,2,2,1]))
