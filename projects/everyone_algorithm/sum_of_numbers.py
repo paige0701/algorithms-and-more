d45dbbf9a21309bc7d66f90a5ce3e38c65e9874d
paige0701 <paigechoi0701@gmail.com>
2019-09-25 21:06:29 +0900

    practice recursive function

--- projects/everyone_algorithm/sum_of_numbers.py
+++ projects/everyone_algorithm/sum_of_numbers.py
@@ -4,5 +4,25 @@ def sum(num):
 
     return sum(num-1)+ num
 
+
+def fact(num):
+    if num == 1:
+        return 1
+    return num * fact(num-1)
+
+
+def find_biggest(li, num):
+    if num == 1:
+        return li[0]
+
+    max = find_biggest(li, num-1)
+    if max > li[num-1]:
+        return max
+    else:
+        return li[num-1]
+
+
+
 if __name__ == '__main__':
     print(sum(5))
+    print(find_biggest([1,52,3,4,5], 5))
