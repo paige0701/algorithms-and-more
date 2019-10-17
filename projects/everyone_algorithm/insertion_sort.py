e01d41bf3b73e123db16b6b7a8fdba8bd1803fe6
paige0701 <paigechoi0701@gmail.com>
2019-10-17 22:37:56 +0900

    insertion sort algorithm

--- projects/everyone_algorithm/insertion_sort.py
+++ projects/everyone_algorithm/insertion_sort.py
@@ -16,5 +16,21 @@ def insertion_sort(a):
             result.insert(idx, temp)
     return result
 
+
+def insertion_sort2(list):
+    for index in range(1, len(list)):
+        value = list[index]
+        i = index-1
+        while i >= 0:
+            if value < list[i]:
+                list[i+1] = list[i]
+                list[i] = value
+                i-=1
+            else:
+                break
+
+    return list
+
 if __name__ == '__main__':
     print(insertion_sort([2,4,5,1,3]))
+    print(insertion_sort2([2, 4, 5, 1, 3]))
