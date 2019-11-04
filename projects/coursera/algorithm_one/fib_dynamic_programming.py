a5e91086faddf6687f764b9b38a7220e4740f60f
paige0701 <paigechoi0701@gmail.com>
2019-11-04 22:42:20 +0900

    solve algorithm : dynamic programming with fibonacci - bottom up

--- projects/coursera/algorithm_one/fib_dynamic_programming.py
+++ projects/coursera/algorithm_one/fib_dynamic_programming.py
@@ -22,7 +22,18 @@ def fib_memo(n):
     memo = [None] * (n+1)
     return fib2(n,memo)
 
+def fib3_bottom_up(n):
+    if n == 1 or n == 2:
+        return 1
+    bottom_up = [None] * (n+1)
+    bottom_up[1] = 1
+    bottom_up[2] = 1
+    for i in range(3, n+1):
+        bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
+    return bottom_up[n]
+
 
 if __name__ == '__main__':
     print(fib_memo(35))
-    print(fib(35))
+    # print(fib(35))
+    print(fib3_bottom_up(35))
