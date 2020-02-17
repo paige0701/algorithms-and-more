46418840228870b1124ad2875132c0b9baa44990
paige0701 <paigechoi0701@gmail.com>
2020-02-17 16:38:18 +0900

    merge sort implementation in python

--- projects/khan_academy/merge_sort.py
+++ projects/khan_academy/merge_sort.py
@@ -27,7 +27,7 @@ compare i and j find smaller number
 [3]
 
 
-increment j
+increment j since j was smaller than i
 
  i           j
  ↓           ↓
@@ -37,7 +37,7 @@ compare i and j get smaller number
 
 [3, 7]
 
-increment i
+increment i since i was smaller than j
     i        j
     ↓        ↓
 [7, 14] [3, 12]
@@ -45,6 +45,47 @@ increment i
 compare i and j get smaller number
 [3, 7, 12, 14]
 
-
-
 """
+
+def merge_sort(arr):
+    if len(arr) > 1:
+
+        mid = len(arr)//2
+
+        low_half = arr[:mid]
+        high_half = arr[mid:]
+
+        merge_sort(low_half)
+        merge_sort(high_half)
+
+
+        i = j = k  = 0
+
+        while i < len(low_half) and j < len(high_half):
+            if low_half[i] < high_half[j]:
+                arr[k] = low_half[i]
+                i += 1
+            else:
+                arr[k] = high_half[j]
+                j += 1
+            k += 1
+
+
+        while i < len(low_half):
+            arr[k] = low_half[i]
+            i += 1
+            k += 1
+
+
+        while j < len(high_half):
+            arr[k] = high_half[j]
+            j += 1
+            k += 1
+
+        return arr
+
+
+
+if __name__ == '__main__' :
+    a = [38, 27, 43, 3, 9, 82, 10]
+    print(merge_sort(a))
