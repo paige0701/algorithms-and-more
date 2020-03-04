d5500362742877428140a07d29fbeb7230193347
paige0701 <paigechoi0701@gmail.com>
2020-03-04 19:56:09 +0900

    solve algorithm: flip and invert image

---
+++ projects/leetCode/easy/flipAndInvertImage.py
@@ -0,0 +1,23 @@
+"""
+Input: [[1,1,0],[1,0,1],[0,0,0]]
+Output: [[1,0,0],[0,1,0],[1,1,1]]
+Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+"""
+class Solution:
+    def flipAndInvertImage(self, A):
+        # for i in A:
+        #     i.reverse()
+        #     print(i)
+        #     for index,val in enumerate(i):
+        #         if i[index] == 0 :
+        #             i[index] = 1
+        #         else:
+        #             i[index] =0
+
+        return [[i-1 for i in row[::-1]] for row in A]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]])
