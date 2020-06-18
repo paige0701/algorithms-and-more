6e2ec7798dda78de6bae610b99e6fd40d32586ee
paige0701 <paigechoi0701@gmail.com>
2020-06-18 23:34:52 +0900

    solve algorithm using greedy

--- projects/inflearn/python_algorithm/binary_search_and_greedy.py
+++ projects/inflearn/python_algorithm/binary_search_and_greedy.py
@@ -296,6 +296,39 @@ class Eight:
         print(cnt)
 
 
+class Nine:
+    def solution(self):
+        import sys
+        sys.stdin = open('bs_and_greedy/nine.txt')
+        n = int(input())
+        a = list(map(int, input().split()))
+        lt = 0
+        rt = n-1
+        last = 0
+        res = ''
+        tmp = []
+
+        while lt<=rt:
+            if a[lt] > last :
+                tmp.append((a[lt], 'L'))
+            if a[rt] > last:
+                tmp.append((a[rt], 'R'))
+            tmp.sort()
+
+            if len(tmp) == 0:
+                 break
+            else:
+                res = res + tmp[0][1]
+                last = tmp[0][0]
+                if tmp[0][1] == 'L' :
+                    lt += 1
+                else:
+                    rt -=1
+
+            tmp.clear()
+        print(res)
+
+
 if __name__ == '__main__':
     one = One()
     # one.binary_search()
@@ -319,4 +352,7 @@ if __name__ == '__main__':
     # seven.solution2()
 
     eight = Eight()
-    eight.solution3()
+    # eight.solution3()
+
+    nine = Nine()
+    nine.solution()
