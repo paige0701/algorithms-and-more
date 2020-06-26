829e967ac5a7ce7c7627288160d52eba8bf265cb
paige0701 <paigechoi0701@gmail.com>
2020-06-26 19:59:41 +0900

    how to create max heap

--- projects/inflearn/python_algorithm/stack_queue_hash_heap.py
+++ projects/inflearn/python_algorithm/stack_queue_hash_heap.py
@@ -344,6 +344,26 @@ class Ten:
 
         print(hq)
 
+    def max_heap(self):
+        import sys
+        import heapq as hq
+        sys.stdin = open('stack_queue_hash_heap/ten.txt')
+        a = []
+        while True:
+            n = int(input())
+            if n == -1:
+                break
+            if n == 0:
+                if len(a) == 0:
+                    print(-1)
+                else:
+                    print(-hq.heappop(a))
+
+            else:
+                hq.heappush(a, -n)
+
+        print(hq)
+
 
 if __name__ == '__main__':
     one = One()
@@ -374,4 +394,4 @@ if __name__ == '__main__':
     # nine.solution()
 
     ten = Ten()
-    ten.min_heap()
+    ten.max_heap()
