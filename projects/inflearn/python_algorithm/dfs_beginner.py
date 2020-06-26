ff93e631eb2614e2d9a520e51d8eaa0f5184de9a
paige0701 <paigechoi0701@gmail.com>
2020-06-27 00:10:31 +0900

    three ways of tree traversal : preorder, inorder, postorder

--- projects/inflearn/python_algorithm/dfs_beginner.py
+++ projects/inflearn/python_algorithm/dfs_beginner.py
@@ -14,6 +14,39 @@ class One:
             print(n%2, end='')
 
 
+class Two:
+    def pre_order_traversal(self, v):
+        if v > 7:
+            return
+        else:
+            print(v, end='')
+            self.pre_order_traversal(v*2)
+            self.pre_order_traversal(v*2 +1)
+
+    def in_order_traversal(self, v):
+        if v > 7:
+            return
+        else:
+            self.in_order_traversal(v * 2)
+            print(v, end='')
+            self.in_order_traversal(v * 2 + 1)
+
+    def post_order_traversal(self, v):
+        if v > 7:
+            return
+        else:
+            self.post_order_traversal(v * 2)
+            self.post_order_traversal(v * 2 + 1)
+            print(v, end='')
+
+
 if __name__ == '__main__':
     one = One()
-    one.to_binary2(11)
+    # one.to_binary2(11)
+
+    two = Two()
+    two.pre_order_traversal(1)
+    print()
+    two.in_order_traversal(1)
+    print()
+    two.post_order_traversal(1)
