31f71410f0577e7a21b77d574780852f3944bf0d
paige0701 <paigechoi0701@gmail.com>
2020-07-13 22:44:11 +0900

    solve algorithm with dfs

---
+++ projects/inflearn/python_algorithm/dfs_bfs/양팔저울.py
@@ -0,0 +1,21 @@
+from idlelib.tree import TreeNode
+
+
+def DFS(L, sum):
+    global res
+    if L == n:
+        if 0 < sum <=s:
+            res.add(sum)
+    else:
+        DFS(L+1, sum+G[L])
+        DFS(L+1, sum-G[L])
+        DFS(L+1, sum)
+
+
+if __name__ == '__main__' :
+    n = 3
+    G = [1, 5, 7]
+    s = sum(G)
+    res = set()
+    DFS(0, 0)
+    print(s-len(res))
