b9fcdce05e7aa47cc47bc63d23639b1f54172061
paige0701 <paigechoi0701@gmail.com>
2020-07-29 15:10:29 +0900

    study dynamic programming

--- projects/inflearn/python_algorithm/dynamicprogramming/네트워크선자르기.py
+++ projects/inflearn/python_algorithm/dynamicprogramming/네트워크선자르기.py
@@ -1,8 +1,28 @@
+def DFS(len):
+    global dy
+
+    if dy[len] > 0:
+        return dy[len]
+
+    if len == 1 or len == 2:
+        return len
+    else:
+        dy[len] = DFS(len-1) + DFS(len-2)
+        return dy[len]
+
+
 if __name__ == '__main__':
+
+    # bottom up
     n = 7
+    # dy = [0] * (n+1)
+    # dy[1] = 1
+    # dy[2] = 2
+    # for i in range(3, n+1):
+    #     dy[i] = dy[i-1] + dy[i-2]
+    # print(dy[n])
+
+    # top down
     dy = [0] * (n+1)
-    dy[1] = 1
-    dy[2] = 2
-    for i in range(3, n+1):
-        dy[i] = dy[i-1] + dy[i-2]
-    print(dy[n])
+    print(dy)
+    print(DFS(n))
