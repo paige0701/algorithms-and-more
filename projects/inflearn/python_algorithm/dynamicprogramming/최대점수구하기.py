eda2e9e24cb7686424698edd1b2dea419f07edbc
paige0701 <paigechoi0701@gmail.com>
2020-08-12 11:06:37 +0900

    improve knapsack algorithm

--- projects/inflearn/python_algorithm/dynamicprogramming/최대점수구하기.py
+++ projects/inflearn/python_algorithm/dynamicprogramming/최대점수구하기.py
@@ -5,6 +5,9 @@ if __name__ == '__main__':
     sys.stdin = open('./최대점수구하기.txt')
 
     n, m = map(int, input().split())
+
+    """
+    using 2 dimensional arrays. -> very slow and takes up a lot of memory 
     ch = [[0] * (m + 1) for i in range(n + 1)]
     for i in range(1, n+1):
         v, t = map(int, input().split())
@@ -14,3 +17,14 @@ if __name__ == '__main__':
             else:
                 ch[i][j] = max((v + ch[i-1][j-t]), ch[i-1][j])
     print(ch[n][m])
+    """
+
+    # using 1 dimensional array
+    dy = [0] * (m+1)
+    for i in range(n):
+        v, t = map(int, input().split())
+        for j in range(m, t-1, -1):
+            dy[j] = max(dy[j-t]+v, dy[j])
+    print(dy[m])
+
+
