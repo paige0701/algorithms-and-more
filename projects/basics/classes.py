a853c59bdcaeb74df98e2783aa66992156235453
paige0701 <paigechoi0701@gmail.com>
2021-03-29 23:14:17 +0900

    study class in python

--- projects/basics/classes.py
+++ projects/basics/classes.py
@@ -14,10 +14,18 @@ class Fraction:
         newnum = self.num * otherfraction.den + self.den * otherfraction.num
         newden = self.den * otherfraction.den
 
-        return Fraction(newnum, newden)
+        common = self.gcd(newnum, newden)
+
+        return Fraction(newnum//common, newden//common)
+
+    @staticmethod
+    def gcd(m, n):
+        while m % n != 0:
+            m, n = n, m % n
+        return n
+
 
 if __name__ == '__main__':
-    fraction = Fraction(3,5)
-    fraction2 = Fraction(1,5)
-    print(fraction + fraction2
-          )
+    fraction = Fraction(3, 5)
+    fraction2 = Fraction(1, 5)
+    print((Fraction(1, 4) + Fraction(1, 2)))
